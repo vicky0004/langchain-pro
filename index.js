37822a1c0239a738d0e629a19d38751411660f0d
vicky0004 <csevicky03@gmail.com>
2025-07-18 12:36:20 +0530

    Feat: gemini LLM integration

--- index.js
+++ index.js
@@ -15,23 +15,43 @@ dotenv.config();
 
 
 // Ollama's ConversationChain with memory
-import { ConversationChain } from "langchain/chains";
-import { ChatOllama } from "@langchain/ollama";
-import { BufferMemory } from "langchain/memory";
+// import { ConversationChain } from "langchain/chains";
+// import { ChatOllama } from "@langchain/ollama";
+// import { BufferMemory } from "langchain/memory";
 
-const model = new ChatOllama({ model: "gemma3:1b" });
+// const model = new ChatOllama({ model: "gemma3:1b" });
 
-const memory = new BufferMemory();
+// const memory = new BufferMemory();
 
-const chain = new ConversationChain({
-  llm: model,
-  memory,
+// const chain = new ConversationChain({
+//   llm: model,
+//   memory,
+// });
+
+// const result = await chain.call({ input: "I love java programming." });
+// console.log(result.response);
+
+// // Call it again to see memory in action
+// const result2 = await chain.call({ input: "give a simple hello world program." });
+// console.log(result2.response);
+
+
+//gemini model
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.0-flash",
+  temperature: 0
 });
 
-const result = await chain.call({ input: "I love java programming." });
-console.log(result.response);
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
-// Call it again to see memory in action
-const result2 = await chain.call({ input: "give a simple hello world program." });
-console.log(result2.response);
+const messages = [
+  new SystemMessage("You are a intractive assistant ."),
+  new HumanMessage("hi how are you!"),
+];
+
+const response = await model.invoke(messages);
+console.log(response);
+
 
